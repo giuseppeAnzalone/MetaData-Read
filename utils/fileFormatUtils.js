c19f231a82165ce8c4e1a912c359c924bfe1d744
Giuseppe <giuseppe.anzalone@digitouch.it>
2025-02-07 16:33:22 +0100

    Semplificata funzione e aggiunti commenti

--- utils/fileFormatUtils.js
+++ utils/fileFormatUtils.js
@@ -1,44 +1,66 @@
 const fs = require('fs').promises;
 
-
-// Funzione per ottenere il formato del file basato sui magic numbers
-const detectFileFormat = async (filePath) => {
-    const magicNumber = await readMagicNumber(filePath, 64); // Leggi i primi 16 byte
-
-    console.log(`Magic Number per ${filePath}:`, magicNumber.toString('hex'));
-
-    if (magicNumber.slice(0, 3).equals(Buffer.from([0xFF, 0xD8, 0xFF]))) {
-        return 'JPEG';
-    } else if (magicNumber.slice(0, 4).equals(Buffer.from([0x89, 0x50, 0x4E, 0x47]))) {
-        return 'PNG';
-    } else if (magicNumber.slice(0, 4).equals(Buffer.from([0x25, 0x50, 0x44, 0x46]))) {
-        return 'PDF';
-    } else if (magicNumber.slice(0, 4).equals(Buffer.from([0x47, 0x49, 0x46, 0x38]))) {
-        return 'GIF';
-    } else if (
-        magicNumber.slice(0, 4).equals(Buffer.from([0x4D, 0x4D, 0x00, 0x2A])) ||  // TIFF big-endian
-        magicNumber.slice(0, 4).equals(Buffer.from([0x49, 0x49, 0x2A, 0x00]))    // TIFF little-endian
-    ) {
-        return 'TIFF';
-    } else if (magicNumber.toString('utf8').includes('<?xml') &&  magicNumber.toString('utf8').includes('<!DOCTYPE html')) {
-        return 'HOCR';
-
-    } else if (magicNumber.slice(0, 4).equals(Buffer.from([0x3C, 0x3F, 0x78, 0x6D]))) {
-        return 'XML';
-    } else {
-        return 'Unknown';
-    }
+const fileFormats = {
+    JPEG:[0xFF, 0xD8, 0xFF],
+    PNG: [0x89, 0x50, 0x4E, 0x47],
+    PDF: [0x25, 0x50, 0x44, 0x46],
+    GIF: [0x47, 0x49, 0x46, 0x38],
+    'TIFF (Big-endian)': [0x4D, 0x4D, 0x00, 0x2A],
+    'TIFF (Little-endian)': [0x49, 0x49, 0x2A, 0x00],
+    XML: [0x3C, 0x3F, 0x78, 0x6D]
 };
 
 
-// Funzione per leggere i primi N byte di un file
+/** 
+ * Funzione per ottenere il formato del file basato sui magic numbers
+ */ 
+const detectFileFormat = async (filePath) => {
+
+    // Legge i primi 64 byte del file
+    const magicNumber = await readMagicNumber(filePath, 64);
+    
+    // Se la lettura fallisce o il buffer è vuoto il formato è considerato Unknown
+    if (magicNumber.length === 0) return 'Unknown';
+
+    // Trova la corrispondenza del formato nel set predefinito fileFormats
+    // Converte l'oggetto fileFormats in un array di coppie [nomeFormato, pattern] usando Object.entries
+    // Usa find() per cercare la prima coppia in cui il pattern dei magic number corrisponde ai primi byte del magicNumber
+    // magicNumber.slice(0, pattern.length) estrae i byte necessari e li confronta con Buffer.from(pattern)
+    const format = Object.entries(fileFormats).find(([_, pattern]) =>
+        magicNumber.slice(0, pattern.length).equals(Buffer.from(pattern))
+    );
+
+    // Restituisce il nome del formato se trova una corrispondenza
+    if (format) return format[0];  // Restituisce il nome del formato (ad esempio 'JPEG')
+
+    // Se il file contiene una stringa in formato UTF-8 che include <?xml e <!DOCTYPE html, il formato viene considerato HOCR
+    if (magicNumber.toString('utf8').includes('<?xml') && magicNumber.toString('utf8').includes('<!DOCTYPE html')) {
+        return 'HOCR';
+    }
+
+    return 'Unknown';
+};
+
+
+/**
+ * Funzione per leggere i primi N byte di un file 
+ */
 const readMagicNumber = async (filePath, numBytes = 64) => {
     try {
+
+        // Apre il file in modalità lettura (r) utilizzando fs.open
         const fileHandle = await fs.open(filePath, 'r');
+        
+        // Alloca un buffer della dimensione specificata (numBytes, 64 byte di default)
         const buffer = Buffer.alloc(numBytes);
+        
+        // Legge i byte dal file e li scrive nel buffer
         await fileHandle.read(buffer, 0, numBytes, 0);
+        
+        // Chiude il file e restituisce il buffer con i byte letti.
         await fileHandle.close();
         return buffer;
+    
     } catch (error) {
         console.error(`⚠️ Errore durante la lettura del file ${filePath}: ${error.message}`);
         return Buffer.alloc(0);
